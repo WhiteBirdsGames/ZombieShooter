e56ede613f3fcae90334ab520a916ae26d4113c3
ZhuDen <zhukowden@gmail.com>
2022-02-09 22:18:47 +0300

    fixed bug time scale for restart level

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/GameManager.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/GameManager.cs
@@ -18,6 +18,7 @@ public class GameManager : MonoBehaviour
 
     public void RestartScene ()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
